a75733d79bf83974f8d4b3a2723bcef9dc323b82
AnomP29 <anom.p.utomo@gmail.com>
2024-04-30 23:12:38 +0700

    dbt_bq_final

---
+++ dags/dbt_bq_final.py
@@ -0,0 +1,63 @@
+from airflow import DAG
+from airflow.providers.cncf.kubernetes.operators.kubernetes_pod import KubernetesPodOperator
+from datetime import datetime, timedelta
+
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 1,
+    'retry_delay': timedelta(minutes=5),
+    'start_date': datetime(2024, 3, 4),
+}
+
+with DAG(
+    'dbt_full_workflow',
+    default_args=default_args,
+    description='A DAG to run dbt tasks including run, seed, and test',
+    schedule_interval=timedelta(days=1),
+    catchup=False,
+) as dag:
+
+    dbt_run = KubernetesPodOperator(
+        namespace='default',
+        image='jrvm/dbt_bigquery:dbt-image',
+        cmds=["dbt", "run"],
+        arguments=["--project-dir", "/dbt_bigquery_main", "--profiles-dir", "/dbt_bigquery_main"],
+        name="dbt_run",
+        task_id="dbt_run",
+        get_logs=True,
+        image_pull_policy='Always',
+        is_delete_operator_pod=True,
+    )
+
+    dbt_seed = KubernetesPodOperator(
+        namespace='default',
+        image='jrvm/dbt_bigquery:dbt-image',
+        cmds=["dbt", "seed"],
+        arguments=["--project-dir", "/dbt_bigquery_main", "--profiles-dir", "/dbt_bigquery_main"],
+        name="dbt_seed",
+        task_id="dbt_seed",
+        get_logs=True,
+        image_pull_policy='Always',
+        is_delete_operator_pod=True,
+    )
+
+    dbt_test = KubernetesPodOperator(
+        namespace='default',
+        image='jrvm/dbt_bigquery:dbt-image',
+        cmds=["dbt", "test"],
+        # Aquí especificas los modelos a excluir en la ejecución del test
+        arguments=["--project-dir", "/dbt_bigquery_main", "--profiles-dir", "/dbt_bigquery_main", "--exclude",
+                   "my_first_dbt_model", "my_second_dbt_model"],
+        name="dbt_test",
+        task_id="dbt_test_task",
+        get_logs=True,
+        dag=dag,
+        is_delete_operator_pod=False,
+        image_pull_policy='Always',
+        security_context={'runAsUser': 0},
+    )
+
+    dbt_run >> dbt_seed >> dbt_test
