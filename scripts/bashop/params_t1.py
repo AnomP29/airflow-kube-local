3ea272ffc268d1ca435c113b9e4171cf9f6a4a93
aprasetyou <aprasetyo@alamisharia.co.id>
2024-06-26 12:13:10 +0700

    params

---
+++ scripts/bashop/params_t1.py
@@ -0,0 +1,15 @@
+from function import main
+
+"""-----------------------------INPUT HERE-----------------------------"""
+
+# Target table
+table_name = 'rw_mis_lawsuit_termohon_levenshtein_score'
+
+# Your query. Make sure you are running on the biqquery console
+query = '''
+{{ params.exec_date }}
+'''
+"""---------------------------------END--------------------------------"""
+
+if __name__ == "__main__":
+  main(table_name, query)
