6d7ed02300422de6a9d9dbfb243d4db704ade169
AnomP29 <49397814+AnomP29@users.noreply.github.com>
2024-06-26 14:29:48 +0700

    Update bashoperator_params.py

--- dags/bashoperator_params.py
+++ dags/bashoperator_params.py
@@ -42,7 +42,6 @@ with DAG(
         tasks[task] = BashOperator(
             task_id=task,
             bash_command= 'echo "coba"',
-            dag=dags,
             )
             # params={
             #     'exec_date': '{{ ds }}'
