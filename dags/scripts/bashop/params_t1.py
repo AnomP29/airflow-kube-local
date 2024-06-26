e539c1554aa86eaba45955d59629e0f5c40d73f6
AnomP29 <49397814+AnomP29@users.noreply.github.com>
2024-06-26 18:44:12 +0700

    Update params_t1.py

--- dags/scripts/bashop/params_t1.py
+++ dags/scripts/bashop/params_t1.py
@@ -7,7 +7,7 @@ table_name = 'rw_mis_lawsuit_termohon_levenshtein_score'
 
 # Your query. Make sure you are running on the biqquery console
 query = '''
-{{ execution_date.strftime('%Y-%m-%dT%H:%M:%S') }}
+{{ params.exec_date }}
 '''
 """---------------------------------END--------------------------------"""
 print(query)
