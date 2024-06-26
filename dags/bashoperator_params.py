37232b6b6946a726cc7c6b066feda2df46337ffc
AnomP29 <49397814+AnomP29@users.noreply.github.com>
2024-06-26 13:09:11 +0700

    Update bashoperator_params.py

--- dags/bashoperator_params.py
+++ dags/bashoperator_params.py
@@ -33,16 +33,16 @@ with DAG(
             order = [o.strip() for o in order.split() if o != ">>"]
             orders_conf.append(order)
 
-    # listed_tasks = set([task for tasks in orders_conf for task in tasks])
+    listed_tasks = set([task for tasks in orders_conf for task in tasks])
 
-    # tasks = {}
-    # for task in listed_tasks:
-    #     tasks[task] = BashOperator(
-    #         task_id=task,
-    #         bash_command="PYTHONPATH={dags} python {dags}/scripts/bashop/{task}.py".format(
-    #             dags=DAGS_FOLDER, task=task
-    #         ),
-    #         params={
-    #             'exec_date': '{{ ds }}'
-    #         }
-    #     )
+    tasks = {}
+    for task in listed_tasks:
+        tasks[task] = BashOperator(
+            task_id=task,
+            bash_command="PYTHONPATH={dags} python {dags}/scripts/bashop/{task}.py".format(
+                dags=DAGS_FOLDER, task=task
+            ),
+            params={
+                'exec_date': '{{ ds }}'
+            }
+        )
