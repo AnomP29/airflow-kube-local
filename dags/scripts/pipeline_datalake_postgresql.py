5cee4cf3e9788db8940d062765e2c8dc58f1b3bc
AnomP29 <49397814+AnomP29@users.noreply.github.com>
2024-08-07 21:20:31 +0700

    Update pipeline_datalake_postgresql.py

--- dags/scripts/pipeline_datalake_postgresql.py
+++ dags/scripts/pipeline_datalake_postgresql.py
@@ -39,3 +39,59 @@ table = options.table
 db = options.db
 schema = options.schema
 dataset = options.dataset
+
+def get_count(conn, schema, table, db_name):
+    # TODO: Ini juga perlu kita sederhanakan logic-nya.
+    # Di sini untuk p2p perlu pakai db_name juga.
+    if (db != 'hijra_staging' and table in ['audit_trail','log_login','anl_user_register','user_lounges','rdl_api_log']) == True:
+#         if db == 'hijra' and table in ['application_activity']:
+#             sql = "SELECT COUNT(*) FROM {}.{} WHERE DATE(insert_date) >= (CURRENT_DATE - INTERVAL '1 DAY')".format(schema,table)
+        # if db_name == 'p2p_prod' and table in ['rdl_api_log']:
+        #     sql = "SELECT COUNT(*) FROM {}.{} WHERE DATE(created_date) >= (CURRENT_DATE - INTERVAL '2 DAY')".format(schema,table)
+        # if db == 'hijra' and table in ['anl_user_register']:
+        #     sql = "SELECT COUNT(*) FROM {}.{} WHERE id != 7934".format(schema,table)
+        # if db == 'hijra' and table in ['user_lounges']:
+        #     sql = "SELECT COUNT(*) FROM {}.{} WHERE id != 7534".format(schema,table)
+        # if db_name == 'p2p_prod' and table in ['log_login']:
+        #     sql = "SELECT COUNT(*) FROM {}.{} WHERE DATE(log_date) >= (CURRENT_DATE - INTERVAL '2 DAY')".format(schema,table)
+        # if db_name == 'p2p_prod' and table in ['audit_trail','application_activity']:
+        #     sql = "SELECT COUNT(*) FROM {}.{} WHERE DATE(activity_date) >= (CURRENT_DATE - INTERVAL '5 DAY')".format(schema,table)
+    else:
+        sql = "SELECT COUNT(*) FROM {}.{}".format(schema,table)
+
+    df = pd.read_sql_query(sql, conn)
+    count = int(str(df['count'].values).replace('[','').replace(']',''))
+
+    return count
+
+def main(db, dataset, schema, table):
+    # DB connect
+    if db in ['p2p_realtime','p2p_prod']:
+        db_host  = db_config.db_p2p_realtime_host
+        db_username = db_config.db_p2p_realtime_username
+        db_password = db_config.db_p2p_realtime_password
+        db_name = db_config.db_p2p_realtime_name
+        db_port = db_config.db_p2p_realtime_port
+
+    conn = psycopg2.connect(
+        host=db_host,
+        user=db_username,
+        password=db_password,
+        database=db_name,
+        port=db_port,
+        connect_timeout=5)
+
+    print("Processing: {}: {}.{}".format(db, schema, table))
+
+    count = get_count(conn, schema, table, db_name)
+    print(count)
+
+    return "SUCCESS"
+
+
+if __name__ == "__main__":
+    main(db, dataset, schema, table)
+
+
+
+
