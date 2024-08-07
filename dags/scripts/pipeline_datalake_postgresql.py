9ac2122909481f7bb5f69fa5489afeb1ccfb9fbc
AnomP29 <49397814+AnomP29@users.noreply.github.com>
2024-08-07 21:23:57 +0700

    Update pipeline_datalake_postgresql.py

--- dags/scripts/pipeline_datalake_postgresql.py
+++ dags/scripts/pipeline_datalake_postgresql.py
@@ -46,8 +46,8 @@ def get_count(conn, schema, table, db_name):
     if (db != 'hijra_staging' and table in ['audit_trail','log_login','anl_user_register','user_lounges','rdl_api_log']) == True:
 #         if db == 'hijra' and table in ['application_activity']:
 #             sql = "SELECT COUNT(*) FROM {}.{} WHERE DATE(insert_date) >= (CURRENT_DATE - INTERVAL '1 DAY')".format(schema,table)
-        # if db_name == 'p2p_prod' and table in ['rdl_api_log']:
-        #     sql = "SELECT COUNT(*) FROM {}.{} WHERE DATE(created_date) >= (CURRENT_DATE - INTERVAL '2 DAY')".format(schema,table)
+        if db_name == 'p2p_prod' and table in ['rdl_api_log']:
+            sql = "SELECT COUNT(*) FROM {}.{} WHERE DATE(created_date) >= (CURRENT_DATE - INTERVAL '2 DAY')".format(schema,table)
         # if db == 'hijra' and table in ['anl_user_register']:
         #     sql = "SELECT COUNT(*) FROM {}.{} WHERE id != 7934".format(schema,table)
         # if db == 'hijra' and table in ['user_lounges']:
