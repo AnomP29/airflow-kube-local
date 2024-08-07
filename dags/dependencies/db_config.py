71397d766c5a8b0e9ead500b86730696480ca229
AnomP29 <49397814+AnomP29@users.noreply.github.com>
2024-08-07 16:00:17 +0700

    Update db_config.py

--- dags/dependencies/db_config.py
+++ dags/dependencies/db_config.py
@@ -1,1 +1,36 @@
+import os
 
+# DB Hijra Dev
+db_hijradev_username = os.environ.get("DB_HIJRADEV_USERNAME")
+db_hijradev_password = os.environ.get("DB_HIJRADEV_PASSWORD")
+db_hijradev_name = os.environ.get("DB_HIJRADEV_NAME")
+db_hijradev_host = os.environ.get("DB_HIJRADEV_HOST")
+db_hijradev_port = os.environ.get("DB_HIJRADEV_PORT")
+
+# DB Hijra Prod
+db_hijraprod_username = os.environ.get("DB_HIJRAPROD_USERNAME")
+db_hijraprod_password = os.environ.get("DB_HIJRAPROD_PASSWORD")
+db_hijraprod_name = os.environ.get("DB_HIJRAPROD_NAME")
+db_hijraprod_host = os.environ.get("DB_HIJRAPROD_HOST")
+db_hijraprod_port = os.environ.get("DB_HIJRAPROD_PORT")
+
+# DB Hijra
+db_hijra_username = os.environ.get("DB_HIJRA_USERNAME")
+db_hijra_password = os.environ.get("DB_HIJRA_PASSWORD")
+db_hijra_name = os.environ.get("DB_HIJRA_NAME")
+db_hijra_host = os.environ.get("DB_HIJRA_HOST")
+db_hijra_port = os.environ.get("DB_HIJRA_PORT")
+
+# DB DWH Hijra
+db_dwh_hijra_username = os.environ.get("DB_DWH_HIJRA_USERNAME")
+db_dwh_hijra_password = os.environ.get("DB_DWH_HIJRA_PASSWORD")
+db_dwh_hijra_name = os.environ.get("DB_DWH_HIJRA_NAME")
+db_dwh_hijra_host = os.environ.get("DB_DWH_HIJRA_HOST")
+db_dwh_hijra_port = os.environ.get("DB_DWH_HIJRA_PORT")
+
+# DB Website Hijra
+db_web_hijra_host  = os.environ.get("DB_WEB_HIJRA_HOST")
+db_web_hijra_username = os.environ.get("DB_WEB_HIJRA_USERNAME")
+db_web_hijra_password = os.environ.get("DB_WEB_HIJRA_PASSWORD")
+db_web_hijra_name = os.environ.get("DB_WEB_HIJRA_NAME")
+db_web_hijra_port = os.environ.get("DB_WEB_HIJRA_PORT")
