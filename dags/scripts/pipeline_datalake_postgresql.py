c1ceee587cb3556f71219e28fe72913c145e6112
AnomP29 <49397814+AnomP29@users.noreply.github.com>
2024-08-07 15:08:20 +0700

    Update pipeline_datalake_postgresql.py

--- dags/scripts/pipeline_datalake_postgresql.py
+++ dags/scripts/pipeline_datalake_postgresql.py
@@ -1,1 +1,16 @@
+import psycopg2
+import pandas as pd
+import optparse
+import numpy as np
+import petl as etl
+import pandas_gbq
+import datetime as dt
+import os
+import pytz
+import sys
 
+from pathlib import Path
+from datetime import datetime
+from google.cloud import bigquery
+from google.api_core.exceptions import NotFound
+from dependencies import db_config
