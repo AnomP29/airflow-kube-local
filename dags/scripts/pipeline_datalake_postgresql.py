e520ddeb77b8655e48a95b98f01d5e794c7f6842
AnomP29 <49397814+AnomP29@users.noreply.github.com>
2024-09-05 15:05:50 +0700

    Update pipeline_datalake_postgresql.py

--- dags/scripts/pipeline_datalake_postgresql.py
+++ dags/scripts/pipeline_datalake_postgresql.py
@@ -66,7 +66,7 @@ def get_count(conn, schema, table, db_name):
 
 def get_data(conn, db, dataset, schema, table, db_name):
     # Object client bigquery cursor
-    client = bigquery.Client('alami-group-data')
+    client = bigquery.Client('hijra-data-dev')
     client.query("""SELECT * FROM {dataset}.{table} LIMIT 1""".format(dataset=dataset,table=table)).result()
 
 
