ad728e0d8f5b2195cc9532ab07069792ecf90749
AnomP29 <49397814+AnomP29@users.noreply.github.com>
2024-09-06 14:58:27 +0700

    Update pipeline_datalake_postgresql.py

--- dags/scripts/pipeline_datalake_postgresql.py
+++ dags/scripts/pipeline_datalake_postgresql.py
@@ -64,9 +64,9 @@ def get_count(conn, schema, table, db_name, date_col, exc_date):
 #         if db == 'hijra' and table in ['application_activity']:
 #             sql = "SELECT COUNT(*) FROM {}.{} WHERE DATE(insert_date) >= (CURRENT_DATE - INTERVAL '1 DAY')".format(schema,table)
         if db_name == 'p2p_prod' and table in ['rdl_api_log']:
-            sql = "SELECT COUNT(*) FROM {}.{} WHERE {} >= {}".format(schema,table,date_col,exc_date)
+            sql = "SELECT COUNT(*) FROM {}.{} WHERE {} >= '{}'".format(schema,table,date_col,exc_date)
         if db == 'hijra' and table in ['anl_user_register']:
-            sql = "SELECT COUNT(*) FROM {}.{} WHERE {} >= {} AND id != 7934".format(schema,table,date_col,exc_date)
+            sql = "SELECT COUNT(*) FROM {}.{} WHERE {} >= '{}' AND id != 7934".format(schema,table,date_col,exc_date)
         # if db == 'hijra' and table in ['user_lounges']:
         #     sql = "SELECT COUNT(*) FROM {}.{} WHERE id != 7534".format(schema,table)
         # if db_name == 'p2p_prod' and table in ['log_login']:
@@ -74,7 +74,7 @@ def get_count(conn, schema, table, db_name, date_col, exc_date):
         # if db_name == 'p2p_prod' and table in ['audit_trail','application_activity']:
         #     sql = "SELECT COUNT(*) FROM {}.{} WHERE DATE(activity_date) >= (CURRENT_DATE - INTERVAL '5 DAY')".format(schema,table)
     else:
-        sql = "SELECT COUNT(*) FROM {}.{} WHERE {} >= {}".format(schema,table,date_col, exc_date)
+        sql = "SELECT COUNT(*) FROM {}.{} WHERE {} >= '{}'".format(schema,table,date_col, exc_date)
 
     print(sql)
     df = pd.read_sql_query(sql, conn)
