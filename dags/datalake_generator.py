b2220760162d21d5d640a547f1687c4d3dad16e9
AnomP29 <49397814+AnomP29@users.noreply.github.com>
2024-09-06 14:51:13 +0700

    Update datalake_generator.py

--- dags/datalake_generator.py
+++ dags/datalake_generator.py
@@ -70,7 +70,7 @@ def create_dag(yml_conf, queue_pool):
                 intval=yml_conf["intval"],
                 intval_unit=yml_conf["intval_unit"],
                 date_col=table["date_col"],
-                exc_date='{{ (execution_date + macros.timedelta(hours=5)).strftime("%Y-%m-%d %H:%M:%s") }}'
+                exc_date='{{ (execution_date + macros.timedelta(hours=5)).strftime("%Y-%m-%d %H:%M") }}'
             )
 
             bash_args = {
