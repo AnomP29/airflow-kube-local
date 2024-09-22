77e002e81560948a4764cf6c02eddc36f250fb7f
AnomP29 <49397814+AnomP29@users.noreply.github.com>
2024-09-22 16:07:12 +0700

    Update datalake_generator.py

--- dags/datalake_generator.py
+++ dags/datalake_generator.py
@@ -97,7 +97,7 @@ def create_dag(yml_conf, queue_pool):
 
                 encryption_command = """\
                 PYTHONPATH={dags} python {dags}/{encryption_script} --db={db} {schema} --dataset={dataset} --table={table} \
-                --date_col={date_col} --exc_date={exc_date}\
+                --date_col={date_col} --exc_date={exc_date} --encr={encr}\
                 """.format(
                     dags=DAGS_FOLDER,
                     encryption_script=encryption_script,
@@ -106,6 +106,7 @@ def create_dag(yml_conf, queue_pool):
                     dataset=yml_conf["dataset"],
                     table=table["name"],
                     date_col=table["date_col"],
+                    encr=table["encryption"],
                     exc_date='{{ (logical_date + macros.timedelta(hours=7)).strftime("%Y-%m-%d/%H:00") }}'
                 )
 
