4822122d0e642d067fb1198c77ec5a070b29f9a1
aprasetyou <aprasetyo@alamisharia.co.id>
2024-10-02 00:31:03 +0700

    oop_bq_002

--- dags/scripts/pipeline_datalake_postgresql.py
+++ dags/scripts/pipeline_datalake_postgresql.py
@@ -269,6 +269,62 @@ def transform_gsheet(dframe, table):
             column_list = " ".join(column_list.split())
             
             return column_select, encrypted_key, column_list
+        
+        else:
+            # df_selected = df
+            df_selected = df.rename(columns={'Column Name':'target_column'})
+            df_selected['data_type'] = 'STRING'
+            df_init = df_selected[['target_column','data_type','Supported Key']]
+            df_raw = df_selected.reset_index(drop=True)
+            df_raw = df_raw.rename(
+                columns={
+                    'Data Type':'type'
+                    ,'Required':'mode'
+                    ,'target_column':'name'
+                    ,'Description':'description'
+                    }
+                )
+            # Get original schema
+            query_string = """
+            SELECT 
+                column_name as target_column, 
+                data_type
+            FROM
+                {dataset}.INFORMATION_SCHEMA.COLUMNS
+            WHERE
+                table_name='{table_name}'""".format(dataset=dataset,table_name=table_name)
+            original_schema = client.query(query_string).to_dataframe()
+
+            # Check table is partition or not
+            query_string = """
+            SELECT 
+                *
+            FROM
+                {dataset}.INFORMATION_SCHEMA.COLUMNS
+            WHERE
+                table_name='{table_name}' AND is_partitioning_column = 'YES'
+            """.format(dataset=dataset,table_name=table_name)
+            is_partition = client.query(query_string).to_dataframe()
+            partition = is_partition
+            
+            result = pd.merge(original_schema, df_init, on=["target_column"], how="left")
+            result.replace(to_replace=[None], value=np.nan, inplace=True)
+            result.fillna(value='', inplace=True)
+            result["data_type_x"] = np.where((result["data_type_y"]==''), result["data_type_x"], result["data_type_y"])
+            
+            
+            # List column to select
+            column_select = result.target_column + ','.strip()
+            column_select = column_select.to_string(header=False,index=False)
+            column_select = " ".join(column_select.split())
+            
+            columns = result.target_column + ' ' + result.data_type_x + ','.strip()
+            column_list = pd.DataFrame(columns).sort_index()
+            column_list = column_list.to_string(header=False,index=False)
+            column_list = " ".join(column_list.split())
+
+            return column_select, '', column_list
+
 
 def main(db, dataset, schema, table, date_col, exc_date):
     # DB connect
