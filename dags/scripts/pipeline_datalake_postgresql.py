c3d9d99acb3eacf4e9ff4ba8cd77230f5bdddffc
AnomP29 <49397814+AnomP29@users.noreply.github.com>
2024-10-03 17:19:32 +0700

    Update pipeline_datalake_postgresql.py

--- dags/scripts/pipeline_datalake_postgresql.py
+++ dags/scripts/pipeline_datalake_postgresql.py
@@ -347,6 +347,8 @@ def main(db, dataset, schema, table, date_col, exc_date):
             get_data(db, dataset, schema, table, db_name, date_col, exc_date)
             column_select, encrypted_key, column_list = transform_gsheet(dframe, tables___)
             bq_operator('hijra-data-dev', dataset, tables___, '', encr, column_select, encrypted_key, column_list)
+        else:
+            raise ValueError('File not parsed completely/correctly')
 
     else:
         tables___ = 'dl__{db}__{schema}__{table}__dev'.format(db=db, schema=schema, table=table)
