e3e6b6334b23a070ba4cafbc327b32a39d7a1203
AnomP29 <49397814+AnomP29@users.noreply.github.com>
2024-10-03 14:22:26 +0700

    Update pipeline_datalake_postgresql.py

--- dags/scripts/pipeline_datalake_postgresql.py
+++ dags/scripts/pipeline_datalake_postgresql.py
@@ -92,7 +92,7 @@ def get_count(schema, table, db_name, date_col, exc_date):
         AND to_char({date_col}, 'YYYY-MM-DD') <= TO_CHAR(TO_TIMESTAMP('{exc_date}', 'YYYY-MM-DD/HH24:MI'), 'YYYY-MM-DD')
         """.format(schema=schema,table=table,date_col=date_col, exc_date=exc_date)
 
-    print(sql)
+    # print(sql)
     df = rdbms_operator('postgres', 'hijra', sql).execute('Y')
     count = int(str(df['count'].values).replace('[','').replace(']',''))
     print(count)
