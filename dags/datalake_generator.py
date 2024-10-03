83fee01e1797f2f543aac68b615d11c548cea99b
AnomP29 <49397814+AnomP29@users.noreply.github.com>
2024-10-03 10:58:56 +0700

    Update datalake_generator.py

--- dags/datalake_generator.py
+++ dags/datalake_generator.py
@@ -30,7 +30,7 @@ def create_dag(yml_conf, queue_pool):
     default_args = {
         "owner": "data_engineer",
         # "start_date": airflow.utils.dates.days_ago(1),
-        "start_date": days_ago(4), 
+        "start_date": days_ago(2), 
         "retries": 1,
         "retry_delay": timedelta(seconds=60),
     }
