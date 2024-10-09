1302e8f19db0f2a2e5928ddba92f1c0829b74ee8
AnomP29 <49397814+AnomP29@users.noreply.github.com>
2024-10-09 15:02:09 +0700

    Update pipeline_datalake_postgresql.py

--- dags/scripts/pipeline_datalake_postgresql.py
+++ dags/scripts/pipeline_datalake_postgresql.py
@@ -162,7 +162,8 @@ def read_gsheet_file(db, dataset, schema, table):
 
     except gspread.exceptions.WorksheetNotFound as e:
         print("Trying to open non-existent sheet. Verify that the sheet name exists (%s)." % table)  
-        df = ''
+        data = []
+        df = pd.DataFrame(data)
 
     # print(df)
     return df  
