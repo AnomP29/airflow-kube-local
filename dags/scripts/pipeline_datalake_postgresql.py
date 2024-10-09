16c556e79a1ba68e118cc47c59fcad8331135eaf
AnomP29 <49397814+AnomP29@users.noreply.github.com>
2024-10-09 09:55:22 +0700

    Update pipeline_datalake_postgresql.py

--- dags/scripts/pipeline_datalake_postgresql.py
+++ dags/scripts/pipeline_datalake_postgresql.py
@@ -190,166 +190,166 @@ def transform_gsheet(dframe, table, src_schema):
     # columns_insert
     
     
-    if "PII" in dframe:
-        if (any(dframe['PII'] == 'TRUE') == True) == True:
-            df_selected = dframe[dframe['PII'] == 'TRUE']
-            df_n = df_selected.copy()
-            df_n['data_type'] = 'BYTES'
-            df_n = df_n.rename(columns={'Column Name':'target_column'})
-            df_init = df_n[['target_column','data_type','Supported Key']]
-            df_inits = list(df_n['target_column'])
+    # if "PII" in dframe:
+    #     if (any(dframe['PII'] == 'TRUE') == True) == True:
+    #         df_selected = dframe[dframe['PII'] == 'TRUE']
+    #         df_n = df_selected.copy()
+    #         df_n['data_type'] = 'BYTES'
+    #         df_n = df_n.rename(columns={'Column Name':'target_column'})
+    #         df_init = df_n[['target_column','data_type','Supported Key']]
+    #         df_inits = list(df_n['target_column'])
             
-            encrypted_key = df_n.head(1)['Encrypted Key'].to_string(index=False)
+    #         encrypted_key = df_n.head(1)['Encrypted Key'].to_string(index=False)
             
-            df_raw = df_n.reset_index(drop=True)
-            df_raw = df_raw.rename(
-                columns={
-                    'Data Type':'type'
-                    ,'Required':'mode'
-                    ,'target_column':'name'
-                    ,'Description':'description'
-                    }
-                )
-            # Get original schema
-            query_string = """
-            SELECT 
-                column_name as target_column, 
-                data_type
-            FROM
-                {dataset}.INFORMATION_SCHEMA.COLUMNS
-            WHERE
-                table_name='{table_name}'""".format(dataset=dataset,table_name=table)
-            original_schema = client.query(query_string).to_dataframe()
+    #         df_raw = df_n.reset_index(drop=True)
+    #         df_raw = df_raw.rename(
+    #             columns={
+    #                 'Data Type':'type'
+    #                 ,'Required':'mode'
+    #                 ,'target_column':'name'
+    #                 ,'Description':'description'
+    #                 }
+    #             )
+    #         # Get original schema
+    #         query_string = """
+    #         SELECT 
+    #             column_name as target_column, 
+    #             data_type
+    #         FROM
+    #             {dataset}.INFORMATION_SCHEMA.COLUMNS
+    #         WHERE
+    #             table_name='{table_name}'""".format(dataset=dataset,table_name=table)
+    #         original_schema = client.query(query_string).to_dataframe()
 
-            # Check table is partition or not
-            query_string = """
-            SELECT 
-                *
-            FROM
-                {dataset}.INFORMATION_SCHEMA.COLUMNS
-            WHERE
-                table_name='{table_name}' AND is_partitioning_column = 'YES'
-            """.format(dataset=dataset,table_name=table)
-            is_partition = client.query(query_string).to_dataframe()
-            partition = is_partition
+    #         # Check table is partition or not
+    #         query_string = """
+    #         SELECT 
+    #             *
+    #         FROM
+    #             {dataset}.INFORMATION_SCHEMA.COLUMNS
+    #         WHERE
+    #             table_name='{table_name}' AND is_partitioning_column = 'YES'
+    #         """.format(dataset=dataset,table_name=table)
+    #         is_partition = client.query(query_string).to_dataframe()
+    #         partition = is_partition
             
-            enc = df_n.drop_duplicates(subset='Encrypted Key', keep="first")
+    #         enc = df_n.drop_duplicates(subset='Encrypted Key', keep="first")
     
-            if not original_schema.empty:
-                result = pd.merge(original_schema, df_init, on=["target_column"], how="left")
-                enc = pd.merge(enc['Encrypted Key'], original_schema, left_on="Encrypted Key", right_on='target_column', how="outer")
-            else:
-                df_emp = dframe[['Column Name', 'Data type']]
-                df_emp = df_emp.rename(columns={'Column Name':'target_column', 'Data type': 'data_type'})
-                result = pd.merge(df_emp, df_init, on=["target_column"], how="left")
-                enc = pd.merge(enc['Encrypted Key'], df_emp, left_on="Encrypted Key", right_on='target_column', how="outer")
+    #         if not original_schema.empty:
+    #             result = pd.merge(original_schema, df_init, on=["target_column"], how="left")
+    #             enc = pd.merge(enc['Encrypted Key'], original_schema, left_on="Encrypted Key", right_on='target_column', how="outer")
+    #         else:
+    #             df_emp = dframe[['Column Name', 'Data type']]
+    #             df_emp = df_emp.rename(columns={'Column Name':'target_column', 'Data type': 'data_type'})
+    #             result = pd.merge(df_emp, df_init, on=["target_column"], how="left")
+    #             enc = pd.merge(enc['Encrypted Key'], df_emp, left_on="Encrypted Key", right_on='target_column', how="outer")
     
-            result.replace(to_replace=[None], value=np.nan, inplace=True)
-            result.fillna(value='', inplace=True)
-            result["data_type_x"] = np.where((result["data_type_y"]==''), result["data_type_x"], result["data_type_y"])
-            result["enc"] = np.where(
-                (result["data_type_y"]=='BYTES'), 
-                '''\
-                AEAD.ENCRYPT(\
-                    (\
-                        SELECT keyset FROM enigma.{table_name}_keys keys \
-                        WHERE keys.{key} = CONCAT(tmptbl.{key}, tmptbl.row_loaded_ts) \
-                    ),CAST(tmptbl. \
-                '''.lstrip().format(
-                        dataset=dataset, 
-                        table_name=table, 
-                        key=encrypted_key, 
-                        target_column=result["target_column"],
-                        supported_key=result["Supported Key"]
-                        ) + result["target_column"] + ' ' +
-                ''' \
-                    AS STRING), CAST( tmptbl.\
-                '''.strip() + result["Supported Key"] + ' ' +
-                ''' \
-                    AS STRING) \
-                ) AS 
-                '''.strip() + ' ' + result['target_column']
-                ,result["target_column"]
-                )
+    #         result.replace(to_replace=[None], value=np.nan, inplace=True)
+    #         result.fillna(value='', inplace=True)
+    #         result["data_type_x"] = np.where((result["data_type_y"]==''), result["data_type_x"], result["data_type_y"])
+    #         result["enc"] = np.where(
+    #             (result["data_type_y"]=='BYTES'), 
+    #             '''\
+    #             AEAD.ENCRYPT(\
+    #                 (\
+    #                     SELECT keyset FROM enigma.{table_name}_keys keys \
+    #                     WHERE keys.{key} = CONCAT(tmptbl.{key}, tmptbl.row_loaded_ts) \
+    #                 ),CAST(tmptbl. \
+    #             '''.lstrip().format(
+    #                     dataset=dataset, 
+    #                     table_name=table, 
+    #                     key=encrypted_key, 
+    #                     target_column=result["target_column"],
+    #                     supported_key=result["Supported Key"]
+    #                     ) + result["target_column"] + ' ' +
+    #             ''' \
+    #                 AS STRING), CAST( tmptbl.\
+    #             '''.strip() + result["Supported Key"] + ' ' +
+    #             ''' \
+    #                 AS STRING) \
+    #             ) AS 
+    #             '''.strip() + ' ' + result['target_column']
+    #             ,result["target_column"]
+    #             )
             
-            enc = enc.dropna()
-            columns_enc = enc.target_column + ' ' + enc.data_type
-            column_list_enc = pd.DataFrame(columns_enc).sort_index()
-            column_list_enc = column_list_enc.to_string(header=False,index=False)
+    #         enc = enc.dropna()
+    #         columns_enc = enc.target_column + ' ' + enc.data_type
+    #         column_list_enc = pd.DataFrame(columns_enc).sort_index()
+    #         column_list_enc = column_list_enc.to_string(header=False,index=False)
             
             
-            # List column to select
-            column_select = result.enc + ','.strip()
-            column_select = column_select.to_string(header=False,index=False)
-            column_select = " ".join(column_select.split())
+    #         # List column to select
+    #         column_select = result.enc + ','.strip()
+    #         column_select = column_select.to_string(header=False,index=False)
+    #         column_select = " ".join(column_select.split())
             
-            columns = result.target_column + ' ' + result.data_type_x + ','.strip()
-            column_list = pd.DataFrame(columns).sort_index()
-            column_list = column_list.to_string(header=False,index=False)
-            column_list = " ".join(column_list.split())
+    #         columns = result.target_column + ' ' + result.data_type_x + ','.strip()
+    #         column_list = pd.DataFrame(columns).sort_index()
+    #         column_list = column_list.to_string(header=False,index=False)
+    #         column_list = " ".join(column_list.split())
         
-            return column_select, encrypted_key, column_list
+    #         return column_select, encrypted_key, column_list
         
-        else:
-            df_selected = dframe.rename(columns={'Column Name':'target_column'})
-            df_selected['data_type'] = df_selected['Data type']
-            df_init = df_selected[['target_column','data_type','Supported Key']]
-            df_raw = df_selected.reset_index(drop=True)
-            df_raw = df_raw.rename(
-                columns={
-                    'Data Type':'type'
-                    ,'Required':'mode'
-                    ,'target_column':'name'
-                    ,'Description':'description'
-                    }
-                )
+    #     else:
+    #         df_selected = dframe.rename(columns={'Column Name':'target_column'})
+    #         df_selected['data_type'] = df_selected['Data type']
+    #         df_init = df_selected[['target_column','data_type','Supported Key']]
+    #         df_raw = df_selected.reset_index(drop=True)
+    #         df_raw = df_raw.rename(
+    #             columns={
+    #                 'Data Type':'type'
+    #                 ,'Required':'mode'
+    #                 ,'target_column':'name'
+    #                 ,'Description':'description'
+    #                 }
+    #             )
     
-            # Get original schema
-            query_string = """
-            SELECT 
-                column_name as target_column, 
-                data_type
-            FROM
-                {dataset}.INFORMATION_SCHEMA.COLUMNS
-            WHERE
-                table_name='{table_name}'""".format(dataset=dataset,table_name=table)
-            original_schema = client.query(query_string).to_dataframe()
+    #         # Get original schema
+    #         query_string = """
+    #         SELECT 
+    #             column_name as target_column, 
+    #             data_type
+    #         FROM
+    #             {dataset}.INFORMATION_SCHEMA.COLUMNS
+    #         WHERE
+    #             table_name='{table_name}'""".format(dataset=dataset,table_name=table)
+    #         original_schema = client.query(query_string).to_dataframe()
     
-            # Check table is partition or not
-            query_string = """
-            SELECT 
-                *
-            FROM
-                {dataset}.INFORMATION_SCHEMA.COLUMNS
-            WHERE
-                table_name='{table_name}' AND is_partitioning_column = 'YES'
-            """.format(dataset=dataset,table_name=table)
-            is_partition = client.query(query_string).to_dataframe()
-            partition = is_partition
+    #         # Check table is partition or not
+    #         query_string = """
+    #         SELECT 
+    #             *
+    #         FROM
+    #             {dataset}.INFORMATION_SCHEMA.COLUMNS
+    #         WHERE
+    #             table_name='{table_name}' AND is_partitioning_column = 'YES'
+    #         """.format(dataset=dataset,table_name=table)
+    #         is_partition = client.query(query_string).to_dataframe()
+    #         partition = is_partition
     
-            if not original_schema.empty:
-                result = pd.merge(original_schema, df_init, on=["target_column"], how="left")
-            else:
-                df_emp = dframe[['Column Name', 'Data type']]
-                df_emp = df_emp.rename(columns={'Column Name':'target_column', 'Data type': 'data_type'})
-                result = pd.merge(df_emp, df_init, on=["target_column"], how="left")
+    #         if not original_schema.empty:
+    #             result = pd.merge(original_schema, df_init, on=["target_column"], how="left")
+    #         else:
+    #             df_emp = dframe[['Column Name', 'Data type']]
+    #             df_emp = df_emp.rename(columns={'Column Name':'target_column', 'Data type': 'data_type'})
+    #             result = pd.merge(df_emp, df_init, on=["target_column"], how="left")
             
-            result.replace(to_replace=[None], value=np.nan, inplace=True)
-            result.fillna(value='', inplace=True)
-            result["data_type_x"] = np.where((result["data_type_y"]==''), result["data_type_x"], result["data_type_y"])
+    #         result.replace(to_replace=[None], value=np.nan, inplace=True)
+    #         result.fillna(value='', inplace=True)
+    #         result["data_type_x"] = np.where((result["data_type_y"]==''), result["data_type_x"], result["data_type_y"])
             
             
-            # List column to select
-            column_select = result.target_column + ','.strip()
-            column_select = column_select.to_string(header=False,index=False)
-            column_select = " ".join(column_select.split())
+    #         # List column to select
+    #         column_select = result.target_column + ','.strip()
+    #         column_select = column_select.to_string(header=False,index=False)
+    #         column_select = " ".join(column_select.split())
             
-            columns = result.target_column + ' ' + result.data_type_x + ','.strip()
-            column_list = pd.DataFrame(columns).sort_index()
-            column_list = column_list.to_string(header=False,index=False)
-            column_list = " ".join(column_list.split())
+    #         columns = result.target_column + ' ' + result.data_type_x + ','.strip()
+    #         column_list = pd.DataFrame(columns).sort_index()
+    #         column_list = column_list.to_string(header=False,index=False)
+    #         column_list = " ".join(column_list.split())
 
-            return column_select, '', column_list, columns_insert
+    #         return column_select, '', column_list, columns_insert
 
 def get_source_schema():
     sql ='''
@@ -391,8 +391,9 @@ def main(db, dataset, schema, table, date_col, exc_date):
     tables___ = 'dl__{db}__{schema}__{table}__dev'.format(db=db, schema=schema, table=table)
     if count != 0:
         src_schema = get_source_schema()
-        # print(src_schema)
+        print(src_schema)
         dframe = read_gsheet_file(db, dataset, schema, table)
+        print(dframe)
         if not dframe.empty:
             # get_data(db, dataset, schema, table, db_name, date_col, exc_date)
             column_select, encrypted_key, column_list, columns_insert = transform_gsheet(dframe, tables___, src_schema)
