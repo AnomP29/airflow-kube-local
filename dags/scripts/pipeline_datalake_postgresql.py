684069ec8dcd1576f17757909d34a1a234dd2cd9
AnomP29 <49397814+AnomP29@users.noreply.github.com>
2024-10-11 11:42:01 +0700

    Update pipeline_datalake_postgresql.py

--- dags/scripts/pipeline_datalake_postgresql.py
+++ dags/scripts/pipeline_datalake_postgresql.py
@@ -121,11 +121,13 @@ def get_data(db, dataset, schema, table, db_name, date_col, exc_date):
     # df = df.fillna(value='', inplace=True)
     df = df.replace('None', '')
     df = df.replace('NaT', '')
+    df = df.replace('nan', '')
     # df['row_loaded_ts'] = pd.Timestamp.now().strftime('%Y-%m-%d %H:%M:%S.%s')
     # df = df[['row_loaded_ts'] + [x for x in df.columns if x != 'row_loaded_ts']]
 
     tables___ = 'dl__{db}__{schema}__{table}__dev'.format(db=db, schema=schema, table=table)
     print('create TEMP tables')
+    print(df)
     try:
         pandas_gbq.to_gbq(df, dataset + '.' + tables___ + '__temp' , project_id='hijra-data-dev',if_exists='append',api_method='load_csv', chunksize=100000)
     except Exception as e:
